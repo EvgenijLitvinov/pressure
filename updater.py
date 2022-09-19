35e218ab5c43710229df373b210d3816291ab4f9
Jack <litvinov.e.v@gmail.com>
2022-09-19 10:17:11 +0300

    изменено:      updater.py

--- updater.py
+++ updater.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 from subprocess import call
-with open('flag.txt', 'r+') as fp:
+
+with open('../pressure/flag.txt', 'r+') as fp:
     flag = fp.read()
     if flag:
-        fp.write(0)
-        call('wget https://github.com/EvgenijLitvinov/pressure/archive/refs/heads/main.zip', shell=True)
-call('press.py')
+        fp.write('\b0')
+        call('curl -O https://github.com/EvgenijLitvinov/pressure/archive/refs/heads/main.zip', shell=True)
+call('./press.py')
 
 print("Content-type: text/html\n")
