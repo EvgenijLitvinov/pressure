5ddd4e8591a196fed29aec50fb4126d11b4fbab2
Jack <litvinov.e.v@gmail.com>
2022-09-30 22:08:23 +0300

    prrr

--- commit&push.sh
+++ commit&push.sh
@@ -4,5 +4,4 @@ read -p "Комментарий для коммита: "
 echo ${REPLY} > flag.txt
 git commit -a -F flag.txt
 git push
-echo "1" > flag.txt
 curl -T flag.txt ftp://y95891gg:Vf4pra2a@y95891gg.beget.tech/y95891gg.beget.tech/public_html/pressure/
--- root/updater.py
+++ root/updater.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 
+import os
 from subprocess import call
 
-with open('../pressure/flag.txt', 'r+') as fp:
-    if fp.read(1) == '1':
-        fp.seek(0)
-        fp.write('0')
-        call('wget -o log https://github.com/EvgenijLitvinov/pressure/archive/refs/heads/main.zip', shell=True)
-        call('unzip main.zip -d main > log', shell=True)
-call('./press.py')
+print("Content-type: text/html\n")
+print('''<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Updater</title>
+</head><body>''')
+
+if os.path.exists('../pressure/flag.txt'):
+    call('wget -o log https://github.com/EvgenijLitvinov/pressure/archive/refs/heads/main.zip', shell=True)
+    call('unzip main.zip > log', shell=True)
+    for file in os.listdir('pressure-main/root'):
+        print(os.stat(file).st_mtime)
+    print(os.stat('updater.py').st_mtime)
+#call('./press.py')
+
+print('</body></html>')
