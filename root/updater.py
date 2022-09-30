5cbd78e9e862f3bc1474d61453778cd33a5834e8
Jack <litvinov.e.v@gmail.com>
2022-09-30 12:20:19 +0300

    upd unzip

--- root/updater.py
+++ root/updater.py
@@ -7,5 +7,5 @@ with open('../pressure/flag.txt', 'r+') as fp:
         fp.seek(0)
         fp.write('0')
         call('wget -o log https://github.com/EvgenijLitvinov/pressure/archive/refs/heads/main.zip', shell=True)
-        call('7z x main.zip -omain > log', shell=True)
+        call('unzip main.zip -d main > log', shell=True)
 call('./press.py')
