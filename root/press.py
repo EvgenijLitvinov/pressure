b06e306d84b9cf714b21e3a21cb5fb9e48869e52
Jack <litvinov.e.v@gmail.com>
2022-12-20 20:23:38 +0200

    fix

--- root/press.py
+++ root/press.py
@@ -112,7 +112,7 @@ print(f'''
 # ------------------------ form modal witn button -------------------------------
 ua_time = datetime.utcnow() + timedelta(hours=2)
 print(f'''<a class="Add" href="#" data-bs-toggle="modal" data-bs-target="#popup">
-        <img src="pressure/plus.png" height="100" width="100">
+        <img src="../pressure/plus.png" height="100" width="100">
         </a>
 <div class="modal fade" id="popup">
 <div class="modal-dialog modal-sm"><form method="post">
